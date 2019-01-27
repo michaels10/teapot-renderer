4565070d0faae7a75b643408a07ff7ebbe16f09c
Rohit Singh <singhrohit2@hotmail.com>
2019-01-27 16:07:14 -0600

    Update images

--- libpyrender/test_void_teapot_frosted_front.py
+++ libpyrender/test_void_teapot_frosted_front.py
@@ -9,7 +9,7 @@ from render import stl_forge, Canvas, Light, add_light, render
 vertices, normals = read_stl("stl/UtahTeapot.stl")
 middle = vertices.mean(0).mean(0)
 vertices -= middle
-scene = stl_forge(vertices+np.array([-5, 0, 3]), normals, scattering=.5, refraction_index=1.8, flip_y=True)
+scene = stl_forge(vertices+np.array([-5, 0, 3]), normals, scattering=.5, refraction_index=1.333, flip_y=True)
 add_light(scene, Light([0,10,-10], 1000))
 canvas = Canvas(300, 300)
 m = render(scene, canvas)
